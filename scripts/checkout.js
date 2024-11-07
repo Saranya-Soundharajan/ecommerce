4d4b949571b26ef73eda37754f229c0243fdd97b
saranya-soundarajan <saranyasoundharajan@gmail.com>
2024-11-06 20:37:54 -0800

    Delete a cartitem from cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart,removeItemFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { priceConvertion } from './utils/priceConversion.js';
 
@@ -38,7 +38,7 @@ cart.forEach((cartItem) => {
             <span class="update-quantity-link link-primary">
                 Update
             </span>
-            <span class="delete-quantity-link link-primary">
+            <span class="delete-quantity-link link-primary js-delete-link" data-product-id = "${matchingProduct.id}">
                 Delete
             </span>
             </div>
@@ -94,3 +94,14 @@ cart.forEach((cartItem) => {
 });
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHtml;
+
+document.querySelectorAll('.js-delete-link')
+.forEach((link) =>{
+    link.addEventListener('click',() => {
+        const productId = link.dataset.productId;  
+        removeItemFromCart(productId);
+
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+        container.remove();
+    });
+});
